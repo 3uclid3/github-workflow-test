cd2bad1da71ed5d2ff9c5223992b361cd2c2f39b
I73v <I73v@pm.me>
2023-07-08 14:52:13 -0400

    Fixed script param and file writing

--- build.sh
+++ build.sh
@@ -5,14 +5,15 @@
 target="x86_64-elf"
 
 # target is overrideable
-if ["$#" -eq 1 ]
-    then target=$1
+if [[ ! -z "$1" ]]
+then 
+    target=$1
 fi
 
 echo "target=$target"
 
 mkdir -p "/home/cross-compiler/$target"
 
-cat <<EOF > target.txt
+cat <<EOF > "/home/cross-compiler/$target/target.txt"
 $target
 EOF
